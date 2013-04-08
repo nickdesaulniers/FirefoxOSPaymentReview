3948d180bc34b4c8ff271abfd0d99b0b98a3484b
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-04-08 09:46:07 -0700

    added manifest

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/titles', title.list);
+app.get('/manifest.webapp', function(req, res) {
+  res.type('application/x-web-app-manifest+json');
+  res.sendfile('manifest.webapp');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
